27f353126107a03a0fd5f6851c78c96abcb6ea01
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-07-01 12:05:14 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api/v1';
+const BASE_URL = 'https://expense-tracker-with-ai-financial-advisor.onrender.com/api/v1';
 
 let tokenStore = {
   accessToken: localStorage.getItem('accessToken') || null,
